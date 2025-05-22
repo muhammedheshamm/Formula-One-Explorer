f3244eae0efb4d2b853ceebeefda5aa7d2d77bd6
Muhammed Hesham <138380445+testomai@users.noreply.github.com>
2025-05-22 23:02:05 +0300

    chore: setup shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './assets/css/app.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button className="cursor-pointer" variant="outline" size="lg" onClick={() => alert('Button clicked!')}>
+        Default
+      </Button>
+    </div>
+  );
 }
 
 export default App;
