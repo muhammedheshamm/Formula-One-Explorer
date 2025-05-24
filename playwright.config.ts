f6b8228be85a27cb4ccdc1806ad9c0ecd0144c44
Muhammed Hesham <muhammedheshamm1@gmail.com>
2025-05-24 16:06:25 +0300

    chore: playwrite setup

---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/e2e',
+  fullyParallel: true,
+  retries: 0,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: true, // Always reuse locally
+    stdout: 'pipe',
+    stderr: 'pipe',
+  },
+});
